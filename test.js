30b939500fc0dcb920821f9632e57acc453dafdc
Xavier Via <fernando.via@gmail.com>
2013-04-16 19:39:18 -0300

    Out of alpha

---
+++ test.js
@@ -0,0 +1,21 @@
+var siter = require('./lib/siter.js');
+
+siter( process.argv[process.argv.length - 1], {
+  image: function (url, site) {
+    console.log(url + " from " + site);
+  },
+
+  video: function (url, site) {
+    console.log(url + " from " + site);
+  },
+
+  noMedia: function (url) {
+    console.log(url);
+  },
+
+  failure: function (error, site) {
+    console.log(error);
+    console.log("In site: " + site);
+    console.log("With URL: " + process.argv[process.argv.length - 1]);
+  }
+});
